86fe743bb6360666ac5e43e517da07f35efc9460
Agu Orcha <aguorcha@gmail.com>
2023-09-02 12:47:23 +0200

    Lógica de navegación implementada con JS

--- main.js
+++ main.js
@@ -1,6 +1,42 @@
 import './styles/styles.scss';
 
-// import imageHeaderMobile from './images/mobile/image-header.jpg';
+const iconMenu = document.querySelector('.mobile-icon-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+iconMenu.addEventListener('click', toggleMenu);
+window.addEventListener('resize', toggleMenu);
+
+function toggleMenu() {
+  const windowWidth = window.innerWidth;
+
+  if (mobileMenu.style.display === 'none') {
+    mobileMenu.style.display = 'block';
+  } else {
+    mobileMenu.style.display = 'none';
+  }
+
+  if (windowWidth > 555) {
+    mobileMenu.style.display = 'none';
+  }
+}
 
 
-// document.querySelector('header').style.backgroundImage = `url(${imageHeaderMobile})`;
+const socialIcons = document.querySelectorAll('.social-icon');
+
+socialIcons.forEach(icon => {
+  icon.addEventListener('mouseenter', () => {
+    const mobileIcon = icon.querySelector('img:not(.hover-icon)');
+    const hoverIcon = icon.querySelector('.hover-icon');
+
+    mobileIcon.style.display = 'none';
+    hoverIcon.style.display = 'block';
+  });
+
+  icon.addEventListener('mouseleave', () => {
+    const mobileIcon = icon.querySelector('img:not(.hover-icon)');
+    const hoverIcon = icon.querySelector('.hover-icon');
+
+    mobileIcon.style.display = 'block';
+    hoverIcon.style.display = 'none';
+  })
+})
