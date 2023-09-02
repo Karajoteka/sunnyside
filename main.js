c0d2dadf7823c02e753175ea802e7506e5809420
Agu Orcha <aguorcha@gmail.com>
2023-09-02 10:57:25 +0200

    Maquetación versión desktop

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 import './styles/styles.scss';
+
+// import imageHeaderMobile from './images/mobile/image-header.jpg';
+
+
+// document.querySelector('header').style.backgroundImage = `url(${imageHeaderMobile})`;
