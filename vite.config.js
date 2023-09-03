f1ddda6cf6a6f0a6c1794e0e871a3d4879075f48
Agu Orcha <aguorcha@gmail.com>
2023-09-03 14:13:53 +0200

    Ajustes de configuración

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-export default {
+import { defineConfig } from "vite";
+
+export default defineConfig ({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
   },
-  base: './',
-};
+  base: '/sunnyside/',
+});
